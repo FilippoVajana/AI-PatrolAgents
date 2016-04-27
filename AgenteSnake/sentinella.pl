67c79ecc264471f23a4f200a60c27a0278a3d23f
Filippo Vajana <Filippo Vajana>
2016-04-27 15:31:41 +0200

    bozza predicato sentinella_avanza

--- AgenteSnake/sentinella.pl
+++ AgenteSnake/sentinella.pl
@@ -10,11 +10,11 @@ type [nord,sud,est,ovest]:punto_cardinale.
      % i 4 ben noti punti cardinali
 
 pred ronda(id, list(punto)).
-	%ronda(ID, -LP) det
-	%definisce un percorso di ronda
+	%ronda(ID, ?LP) det
+	%definisce un percorso di ronda attraverso i suoi punti di passaggio
 
 pred sentinella(id, ronda).
-	%sentinella(+ID,?R) definisce una sentinella tramite nome e lista punti di passaggio
+	%sentinella(+ID,?R) definisce una sentinella tramite nome e e identificativo della ronda in corso
 
 pred posizione_sentinella(sentinella, punto).
 	%posizione_sentinella(+S,+P)
@@ -24,8 +24,6 @@ pred direzione_cammino_sentinella(id_sentinella, punto_cardinale).
 	%direzione_cammino_sentinella(+ID_S,-DS)
 	%restituisce la direzione in cui la sentinella S sta guardando
 
-
-
 pred giocatore(nome, punto).
 	%giocatore(+G,+POS)
 	%descrive un giocatore tramite nome e posizione
@@ -34,17 +32,48 @@ pred giocatore_avvistato(id_giocatore, id_sentinella).
 	%giocatore_avvistato(+G,?S)
 	%rileva se il giocatore G è stato avvistato da una sentinella S
 
+pred sentinella_avanza(id_sentinella).
+	%sentinella_avanza(+ID_S)
+	%fa avanzare la sentinella ID_S lungo il suo percorso di ronda
 %%	IMPLEMENTAZIONI	%%
 
-ronda(r1, [p(0,0),p(5,0),p(5,5),p(0,5)]).%da implementare
-ronda(r2, [p(0,0),p(15,0),p(15,15),p(0,15)]).%da implementare
+pred clock(integer).
+	%clock()
+	%il tempo della simulazione avanza di un tick
 
 
-sentinella(s1, ronda(r1,PP)) :- ronda(r1,PP).
+sim_clock().
+:-dynamic(sim_clock/1).
+
+clock(Tasd) :-
+	T is sim_clock(T),
+	T_Next is T + 1,
+	assertz(sim_clock(T_Next)),
+	retract(sim_clock(T)).
+
+
+sentinella_avanza(ID_S) :-
+	clock(asd),
+	sentinella(ID_S, ronda(ID_R,[PP_Head|PP_Tail])),
+	direzione_cammino_sentinella(ID_S, nord),
+
+	assertz(posizione_sentinella(ID_S,p(XS + 1,YS))),
+	retract(posizione_sentinella(ID_S,p(XS,YS))),
+
+	retract(sentinella(ID_S, _)),
+	assertz(sentinella(ID_S, ronda(ID_R,[PP_Tail|PP_Head]))).
+
+	
+
+
+
+ronda(r1, [p(0,0),p(5,0),p(5,5),p(0,5)]).
+ronda(r2, [p(0,0),p(15,0),p(15,15),p(0,15)]).
+
+sentinella(s1, ronda(r1,PP)) :- ronda(r1,PP).% da rivedere per benino
 sentinella(s2, ronda(r2,PP)) :- ronda(r2,PP).
 
-posizione_sentinella(s1,p(1,0)).% LPP = lista punti di passaggio
-posizione_sentinella(s2,p(1,2)).
+posizione_sentinella(s1,p(0,0)).
 
 direzione_cammino_sentinella(ID_S, nord) :-
 	posizione_sentinella(ID_S, p(X_S,Y_S)),
@@ -114,7 +143,7 @@ pred avvistato(giocatore, sentinella).
 %%	Spec: vero sse S Ã¨ la sentinella che ha avvistato l'agente
 
 avvistato(p(SX,SY),p(SENTX,SENTY),NAME) :-
-  sentinella_dove(p(SENTX,SENTY),_,NAME) %% NOTA da implementare
+  sentinella_dove(p(SENTX,SENTY),_,NAME), %% NOTA da implementare
   area_sentinella(p(SENTX,SENTY),NAME, area(p(X1,Y1),p(X2,Y2))),
   punto_area(p(SX,SY),area(p(X1,Y1),p(X2,Y2))).
 
