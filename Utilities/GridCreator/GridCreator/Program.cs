1c4c771eda38746979293e70b1e49ea4a6b2e6f1
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2016-05-09 18:02:09 +0200

    implementati metodi per aggiunta regole : id, dimensioni, ostacoli

--- Utilities/GridCreator/GridCreator/Program.cs
+++ Utilities/GridCreator/GridCreator/Program.cs
@@ -11,7 +11,7 @@ namespace GridCreator
     {
         static void Main(string[] args)
         {
-            List<String> inputLines = new List<string>();
+            List<string> inputLines = new List<string>();
 
             if (args.Length != 0)
             {
@@ -25,20 +25,26 @@ namespace GridCreator
                 inputLines = ReadFile("Grid_0.txt");
             }
 
+            List<string> gridRulesList = MakeGrid(inputLines);
+
             //debug
-            foreach (String s in inputLines)
+            System.Console.WriteLine("Original Grid");
+            foreach (string s in inputLines)
                 System.Console.WriteLine(s);
 
+            System.Console.WriteLine("Grid Rules");
+            foreach (string rule in gridRulesList)
+                System.Console.WriteLine(rule);
             //stop
             System.Console.ReadLine();
         }
 
-        private static List<String> ReadFile(string path)
+        private static List<string> ReadFile(string path)
         {
             FileStream fileStream = File.Open(path, FileMode.Open);
             StreamReader reader = new StreamReader(fileStream);
 
-            List<String> inputLines = new List<string>();
+            List<string> inputLines = new List<string>();
 
             while (!reader.EndOfStream)
                 inputLines.Add(reader.ReadLine());
@@ -46,10 +52,54 @@ namespace GridCreator
             return inputLines;            
         }
 
-        private static void MakeGrid()
+        private static List<string> MakeGrid(List<string> inputLines)
         {
+            int gridHeight = inputLines.Count, gridWidth = inputLines.ElementAt(0).Length;
+            List<string> gridRulesList = new List<string>();
 
+            //inserisco regola id griglia
+            AddGridIdRule(gridRulesList);
+            //inserisco regola dimensione griglia
+            AddGridDimensionRule(gridHeight, gridWidth, gridRulesList);
+
+            //inserisco regola entità
+            int l = 0;
+            foreach(string line in inputLines)
+            {
+                char[] _lineArray = line.ToCharArray();
+                for(int c=0; c<_lineArray.Length; c++)
+                {
+                    if (_lineArray[c].Equals('#'))
+                        AddObstaclesRule(c, l, gridRulesList);
+                }
+                l++;
+            }
+
+            return gridRulesList;
         }
+
+        private static void AddGridIdRule(List<string> gridRulesList)
+        {
+            Random randomNum = new Random(DateTime.Now.Second);
+            string gridIdRule = $"id_campo({randomNum.Next()}).";
+            gridRulesList.Add(gridIdRule);
+        }
+
+        private static void AddGridDimensionRule(int height, int width, List<string> gridRulesList)
+        {
+            string pNE = string.Format("p(0,{0})", height);
+            string pSO = string.Format("p({0},0)", width);
+            string dimensionRule = $"area_griglia(area({pNE},{pSO})).";
+            gridRulesList.Add(dimensionRule);
+        }
+
+        private static void AddObstaclesRule(int x, int y, List<string> gridRulesList)
+        {            
+            string obstacleRule = $"entita_gioco(ostacolo, p({x},{y})).";
+            gridRulesList.Add(obstacleRule);
+        } 
+
+
 
         private static void WriteFile(string path)
         {
