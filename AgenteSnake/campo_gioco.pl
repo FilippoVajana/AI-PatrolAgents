1d8eb3d94eb41adbf1b3a2b4cc4e6007d40c8e3c
Filippo Vajana <Filippo Vajana>
2016-05-09 16:26:00 +0200

    inizio implementazione programma ausiliario per creazione file griglia

--- AgenteSnake/campo_gioco.pl
+++
@@ -1,8 +0,0 @@
-%% PRIMA BOZZA PER LA GESTIONE E LA STAMPA DEL CAMPO DI GIOCO %%
-
-type CAMPO
-type dimensioni x e y
-type simboli
-
-pred stampa riga
-pred stampa colonna
---
+++ Utilities/GridCreator/GridCreator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            if(File.Exists(args[0]))
+            {
+                
+            }
+        }
+
+        private void ReadFile(string path)
+        {
+            
+        }
+
+        private void MakeGrid()
+        {
+
+        }
+
+        private void WriteFile(string path)
+        {
+
+        }
+    }
+}
